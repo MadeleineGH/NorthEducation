514bcd8eb5c6ef03136fc4983bb98a4948b18535
MadeleineGH <89448074+MadeleineGH@users.noreply.github.com>
2022-06-08 10:15:51 +0200

    Update

--- Education-API/Data/Migrations/20220608081234_Init.Designer.cs
+++ Education-API/Data/Migrations/20220608081234_Init.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Education_API.Data.Migrations
 {
     [DbContext(typeof(EducationContext))]
-    [Migration("20220607164422_Init")]
+    [Migration("20220608081234_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
